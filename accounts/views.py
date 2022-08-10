d06551009ce01a04b33ee09dcbf05917550700d6
Kerriea-star <ojared511@gmail.com>
2022-08-10 15:44:04 +0300

    Customer registration viewset

--- accounts/views.py
+++ accounts/views.py
@@ -50,3 +50,31 @@ class LoginViewSet(ModelViewSet, TokenObtainPairView):
         except TokenError as e:
             raise InvalidToken(e.args[0])
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class CustomerRegistrationViewSet(ModelViewSet, TokenObtainPairView):
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
+    http_method_names = ['post',]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        user.role = "Customer"
+        user.is_active = False
+        user.save()
+        Customer.objects.update_or_create(user=user)
+        user_data = serializer.data
+        send_activation_mail(user_data, request)
+
+        refresh = RefreshToken.for_user(user)
+        res = {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+        return Response({
+            "user": serializer.data,
+            "refresh": res['refresh'],
+                        "token": res['access']
+                        }, status=status.HTTP_201_CREATED)
