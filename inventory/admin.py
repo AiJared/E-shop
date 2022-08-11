11bfd1f5d20008348ba9d71f4def515fe2d1513b
Kerriea-star <ojared511@gmail.com>
2022-08-11 10:45:43 +0300

    inventory admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from inventory.models import Category, Product, Rating
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Rating)
