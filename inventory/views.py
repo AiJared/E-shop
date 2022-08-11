7d687daccb359e1f6e4a2c8f7d2a0d3c7fb00083
Kerriea-star <ojared511@gmail.com>
2022-08-11 15:55:12 +0300

    CategoryAPIView

--- inventory/views.py
+++ inventory/views.py
@@ -87,3 +87,57 @@ class ProductAPIView(ModelViewSet):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
+class CategoryAPIView(ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdministrator, IsCustomer]
+    http_method_names = ["get", "post", "put", "patch", "delete"]
+    queryset = Category.obejcts.all()
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if len(queryset) > 0:
+            query = request.GET.get('q')
+            if query:
+                queryset = queryset.filter(
+                    Q(category__icontains=query)
+                )
+            paginator = CustomPagination()
+            result_page = paginator.paginate_queryset(queryset, request)
+            serializer = self.get_serializer(result_page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        queryset = self.get_queryset()
+        queryset = get_object_or_404(queryset, pk=pk)
+        serializer = self.get_serializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializere(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if request.user.role == "Administrator":
+            serializer.save()
+        else:
+            return Response(
+                {"message": "You are not authorized to perform this action."},
+                status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk=None, *args, **kwargs):
+        queryset = self.get_queryset()
+        queryset = get_object_or_404(queryset, pk=pk)
+        serializer = self.get_serializer(
+            queryset, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        if request.user.role == "Administrator":
+            serializer.save()
+        else:
+            return Response(
+                {"message": "You are not authorized to perform this action."},
+                status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def destroy(self, request, pk=None, *args, **kwargs):
+        queryset = self.get_queryset()
+        queryset = get_object_or_404(queryset, pk=pk)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
