4e3c29fc9e4046e59a94c2f864562e5fb5d25fa1
Kerriea-star <ojared511@gmail.com>
2022-08-11 11:10:08 +0300

    inventory serializer

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -14,3 +14,38 @@ class CategorySerializer(ModelSerializer):
         read_only_fields = ("id",)
 
 
+class ProductSerializer(ModelSerializer):
+    category = CategorySerializer(read_only=True, required=False)
+
+    class Meta:
+        model = Product
+        fields = ("id", "product_name", "uint_price"
+                    "stock", "description", "category",
+                    "image", "created_at", "updated_at")
+
+        read_only_fields = ("id",)
+
+    # def update(self, instance, validated_data):
+    #     print("update::::")
+    #     if validated_data.get('category'):
+    #         category_data = validated_data.get('category')
+    #         category_serializer = CategorySerializer(data=category_data)
+
+    #         if category_serializer.is_valid():
+    #             print("Validity::::", category_serializer.validated_data)
+    #             category = category_serializer.update(instance=instance.category,
+    #                                                   validated_data=category_serializer.validated_data)
+    #             validated_data['category'] = category
+
+    #     return super().update(instance, validated_data)
+
+    
+class RatingSerializer(ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = Rating
+        fields = ("id", "product", "rating",
+                    "review", "customer", "created_at",
+                    "updated_at",)
+        read_only_fields = ("id")
