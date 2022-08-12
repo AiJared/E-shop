2849e65a17de2f4cb4fd60862fa82c7257744715
Kerriea-star <ojared511@gmail.com>
2022-08-12 11:18:12 +0300

    views imports

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from functools import partial
+from django.shortcuts import render, get_object_or_404
+from accounts.models import Customer
+from orders.models import Order
+from orders.serializers import OrderSerializer
+from rest_framework import generics, serializers, status, viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from accounts.permissions import IsAdministrator, IsCustomer
+from django.db.models import Q
 
-# Create your views here.
