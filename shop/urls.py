953cf800d160ab23d0ea7f47c16e538b092a4db3
Kerriea-star <ojared511@gmail.com>
2022-08-12 12:20:00 +0300

    project urls

--- shop/urls.py
+++ shop/urls.py
@@ -2,9 +2,32 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-shop API",
+        default_version="v1",
+        description="A simple Inventory Management API",
+        terms_of_service="",
+        contact=openapi.Contact(email="ojared511@gmail.com"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/v1/", include('api.urls')),
+    path('', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-redoc'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
